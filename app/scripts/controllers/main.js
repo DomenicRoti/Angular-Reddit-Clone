62343c1decc2703929c6875d79be974e70a9b583
DomenicRoti <domenic.roti@gmail.com>
2014-12-30 17:50:02 -0600

    Adding intial links to the posts on the front page.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,34 @@
  * Controller of the redditCloneApp
  */
 angular.module('redditCloneApp')
-  .controller('MainCtrl', function ($scope, Posts, $cookies) {
+  .controller('MainCtrl', function ($scope, Posts, $cookies, $http) {
 
     Posts.getPosts(function(results){
       console.log(results.children);
       $scope.posts = results.children;
     });
+
+    $scope.getPostLink = function(postData){
+      if(postData.domain.substring(0,4) == "self"){
+        return "#/post/" + postData.id;
+      } else {
+        return postData.url;
+      }
+    };
+
+    //
+    // var authHeaderString = "bearer " + $cookies.accesstoken;
+    //   var req = {
+    //     method: 'GET',
+    //     url: 'https://oauth.reddit.com/api/multi/mine',
+    //     headers: {
+    //      'Authorization': authHeaderString
+    //     }
+    //   }
+
+    //   $http(req).success(function(data){
+    //     console.log(data);
+    //   }).error(function(){
+    //   });
 
   });
